e4c6e5f0c4907dbad5d07476e32d2459f2f99f72
Tebello Keneth Nhlapo <NhlapoT@arena.africa>
2024-04-17 12:38:16 +0200

    Added the IVS and fixed the Chatbot
    Added the IVS and fixed the chatbot

--- Subs.Presentation/Properties/Settings.Designer.cs
+++ Subs.Presentation/Properties/Settings.Designer.cs
@@ -47,7 +47,7 @@ namespace Subs.Presentation.Properties {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
             "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
-            "tring>VANDERMERWER</string>\r\n</ArrayOfString>")]
+            "tring>VANDERMERWER</string>\r\n  <string>NHLAPOT</string>\r\n</ArrayOfString>")]
         public global::System.Collections.Specialized.StringCollection Authority3 {
             get {
                 return ((global::System.Collections.Specialized.StringCollection)(this["Authority3"]));
@@ -67,9 +67,12 @@ namespace Subs.Presentation.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
-            "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
-            "tring>REITMANNH</string>\r\n  <string>HEIN</string>\r\n</ArrayOfString>")]
+        [global::System.Configuration.DefaultSettingValueAttribute(@"<?xml version=""1.0"" encoding=""utf-16""?>
+<ArrayOfString xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
+  <string>REITMANNH</string>
+  <string>HEIN</string>
+  <string>NHLAPOT</string>
+</ArrayOfString>")]
         public global::System.Collections.Specialized.StringCollection Authority4 {
             get {
                 return ((global::System.Collections.Specialized.StringCollection)(this["Authority4"]));
