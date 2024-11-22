93e7404d279cde6a0398ba7357133dd6784a9479
TK NHLAPO <61691468+NhlapoT@users.noreply.github.com>
2024-11-22 12:52:12 +0200

    I have added the Redirect
    We added the redirect so that after 10 seconds it can redirect to the main page to be user friendly

--- Subs.MimsWeb/Controllers/HomeController.cs
+++ Subs.MimsWeb/Controllers/HomeController.cs
@@ -1235,8 +1235,8 @@ namespace Subs.MimsWeb.Controllers
 
                 int lToken = (int)lLoginRequest.CustomerId * 88 * (DateTime.Now.Hour + 1);
 
-                return Redirect("https://drugchecker.mims.co.za/?token_id" + lToken.ToString());
-                return Redirect("https://www.mimscpd.co.za?Id=" + lToken.ToString());
+                return Redirect("https://drugchecker.mims.co.za/?token_id=" + lToken.ToString());
+               
 
             }
             catch (Exception ex)
